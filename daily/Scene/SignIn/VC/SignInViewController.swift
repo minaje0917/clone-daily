9367ebb12fbd9dced9f92ae89e9352b1ed3abc24
minaje0917 <k050918@naver.com>
2022-10-31 18:20:19 +0900

    :sparkles: add SignInText, SubText

--- daily/Scene/SignIn/VC/SignInViewController.swift
+++ daily/Scene/SignIn/VC/SignInViewController.swift
@@ -23,8 +23,20 @@ class SignInViewController: BaseViewController<SignInViewModel> {
         $0.image = UIImage(named: "SignInBackground.png")
     }
     
+    let signInText = UILabel().then {
+        $0.text = "Login"
+        $0.font = UIFont.systemFont(ofSize: 36, weight: .heavy)
+        $0.textColor = UIColor(red: 166 / 255, green: 73 / 255, blue: 141 / 255, alpha: 1.00)
+    }
+    
+    let subText = UILabel().then {
+        $0.text = "Welcome Back!"
+        $0.font = UIFont.systemFont(ofSize: 24, weight: .medium)
+        $0.textColor = UIColor(red: 166 / 255, green: 73 / 255, blue: 141 / 255, alpha: 1.00)
+    }
+    
     override func addView() {
-        [backImage].forEach {
+        [backImage,signInText,subText].forEach {
             view.addSubview($0)
         }
     }
@@ -33,6 +45,14 @@ class SignInViewController: BaseViewController<SignInViewModel> {
         backImage.snp.makeConstraints {
             $0.top.equalTo(view.snp.top).offset(0)
             $0.leading.trailing.equalToSuperview().offset(0)
+        }
+        signInText.snp.makeConstraints {
+            $0.top.equalTo(view.snp.top).offset((bounds.height) / 5.75)
+            $0.centerX.equalToSuperview()
+        }
+        subText.snp.makeConstraints {
+            $0.top.equalTo(signInText.snp.bottom).offset(8)
+            $0.centerX.equalToSuperview()
         }
     }
 
