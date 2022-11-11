b1b0b2f37b200854d56170dde2efcbc90a3b2578
minaje0917 <k050918@naver.com>
2022-11-11 10:11:13 +0900

    :sparkles: add forgotPwButton, signInButton, backSignUpButton

--- daily/Scene/SignIn/VC/SignInViewController.swift
+++ daily/Scene/SignIn/VC/SignInViewController.swift
@@ -55,9 +55,35 @@ class SignInViewController: BaseViewController<SignInViewModel> {
     let pwUnderLine = UIView().then {
         $0.backgroundColor = UIColor(red: 250/255, green: 194/255, blue: 215/255, alpha: 1.00)
     }
+    
+    lazy var forgotPwButton = UIButton().then {
+        let text = NSAttributedString(string: "Forgot Password?")
+        $0.setAttributedTitle(text, for: .normal)
+        $0.titleLabel?.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        $0.setTitleColor(UIColor(red: 0, green: 0, blue: 0, alpha: 1.00), for: .normal)
+        $0.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0)
+    }
+    
+    lazy var signInButton = UIButton().then {
+        let text = NSAttributedString(string: "Login")
+        $0.setAttributedTitle(text, for: .normal)
+        $0.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
+        $0.setTitleColor(UIColor(red: 191/255, green: 74/255, blue: 160/255, alpha: 1.00), for: .normal)
+        $0.backgroundColor = UIColor(red: 250/255, green: 194/255, blue: 215/255, alpha: 1.00)
+        $0.layer.cornerRadius = 30
+    }
 
+    lazy var backSignUpButton = UIButton().then {
+        let text = NSAttributedString(string: "Don’t have an account?")
+        $0.setAttributedTitle(text, for: .normal)
+        $0.titleLabel?.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        $0.setTitleColor(UIColor(red: 192/255, green: 192/255, blue: 192/255, alpha: 1.00), for: .normal)
+        $0.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0)
+    }
+    
     override func addView() {
-        [backImage,signInText,subText,emailTextField,emailUnderLine,pwTextField,pwUnderLine].forEach {
+        [backImage,signInText,subText,emailTextField,emailUnderLine,pwTextField,pwUnderLine
+        ,forgotPwButton,signInButton,backSignUpButton].forEach {
             view.addSubview($0)
         }
     }
@@ -97,7 +123,21 @@ class SignInViewController: BaseViewController<SignInViewModel> {
             $0.top.equalTo(pwTextField.snp.bottom).offset(10)
             $0.trailing.leading.equalToSuperview().inset(16)
         }
-        
+        forgotPwButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(pwUnderLine.snp.bottom).offset(6)
+        }
+        signInButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.leading.equalTo(view.snp.leading).offset(16)
+            $0.height.equalTo(56)
+            $0.top.equalTo(forgotPwButton.snp.bottom).offset((bounds.height) / 9.90)
+        }
+        backSignUpButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset((bounds.width) / 3.40)
+            $0.top.equalTo(signInButton.snp.bottom).offset(8)
+        }
     }
 
 }
