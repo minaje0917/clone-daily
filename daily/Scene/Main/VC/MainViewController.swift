042def8d0c041a8da24652fd704c31b1cc6db69b
minaje0917 <k050918@naver.com>
2022-11-14 15:44:18 +0900

    :sparkles: add toolbar

--- daily/Scene/Main/VC/MainViewController.swift
+++ daily/Scene/Main/VC/MainViewController.swift
@@ -14,33 +14,84 @@ class MainViewController: BaseViewController<MainViewModel> {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        self.navigationController?.isToolbarHidden = false
+//        self.navigationController?.isToolbarHidden = false
         self.navigationItem.hidesBackButton = true
-        toolbar()
     }
     
-    func toolbar() {
-        let showDaily =  UIBarButtonItem(title: "일기보기", style: .plain, target:self, action: nil)
-        let showProfile = UIBarButtonItem(title: "프로필보기", style: .plain, target: self, action: nil)
-        let showRoom = UIBarButtonItem(title: "구경하기", style: .plain, target: self, action: nil)
-        var items = [UIBarButtonItem]()
-        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
-
-        [showDaily,flexibleSpace,showProfile,flexibleSpace,showRoom].forEach {
-            items.append($0)
-        }
-        self.toolbarItems = items
-        toolbarItems = [showDaily,flexibleSpace,showProfile,flexibleSpace,showRoom]
+//    lazy var showDailyButton = UIButton().then {
+//        $0.setImage(UIImage(systemName: "book"), for: .normal)
+//        let text = NSAttributedString(string: "일기보기")
+//        $0.setAttributedTitle(text, for: .normal)
+//        $0.titleLabel?.font = UIFont.systemFont(ofSize: 10, weight: .medium)
+//        $0.imageEdgeInsets = UIEdgeInsets(top: 30, left: 30, bottom: 30, right: 30)
+//    }
+    
+//    func toolbar() {
+//        var showDaily =  UIBarButtonItem(customView: showDailyButton)
+//        let showProfile = UIBarButtonItem(title: "프로필보기", style: .plain, target: self, action: nil)
+//        let showRoom = UIBarButtonItem(title: "구경하기", style: .plain, target: self, action: nil)
+//        var items = [UIBarButtonItem]()
+//        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+//
+//        [showDaily,flexibleSpace,showProfile,flexibleSpace,showRoom].forEach {
+//            items.append($0)
+//        }
+//        self.toolbarItems = items
+//        toolbarItems = [showDaily,flexibleSpace,showProfile,flexibleSpace,showRoom]
+//    }
+    
+    lazy var dailyButton = UIButton().then {
+        $0.setImage(UIImage(systemName: "book"), for: .normal)
+        let text = NSAttributedString(string: "일기보기")
+        $0.setAttributedTitle(text, for: .normal)
+        $0.titleLabel?.font = UIFont.systemFont(ofSize: 10, weight: .medium)
+        $0.setTitleColor(UIColor(red: 0/255, green: 122/255, blue: 255/255, alpha: 1.00), for: .normal)
+        $0.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1.00)
+    }
+    
+    lazy var profileButton = UIButton().then {
+        $0.setImage(UIImage(systemName: "person"), for: .normal)
+        let text = NSAttributedString(string: "프로필보기")
+        $0.setAttributedTitle(text, for: .normal)
+        $0.titleLabel?.font = UIFont.systemFont(ofSize: 10, weight: .medium)
+        $0.setTitleColor(UIColor(red: 0/255, green: 122/255, blue: 255/255, alpha: 1.00), for: .normal)
+        $0.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1.00)
+    }
+    
+    lazy var roomButton = UIButton().then {
+        $0.setImage(UIImage(systemName: "binoculars"), for: .normal)
+        let text = NSAttributedString(string: "구경하기")
+        $0.setAttributedTitle(text, for: .normal)
+        $0.titleLabel?.font = UIFont.systemFont(ofSize: 10, weight: .medium)
+        $0.setTitleColor(UIColor(red: 0/255, green: 122/255, blue: 255/255, alpha: 1.00), for: .normal)
+        $0.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1.00)
     }
     
     override func addView() {
-        [].forEach {
+        [dailyButton,profileButton,roomButton].forEach {
             view.addSubview($0)
         }
     }
 
     override func setLayout() {
-        
+        dailyButton.snp.makeConstraints {
+            $0.height.equalTo(56)
+            $0.width.equalTo(125)
+            $0.leading.equalTo(view.snp.leading).offset(0)
+            $0.bottom.equalTo(view.snp.bottom).inset(27)
+        }
+        profileButton.snp.makeConstraints {
+            $0.height.equalTo(56)
+            $0.width.equalTo(125)
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalTo(view.snp.bottom).inset(27)
+        }
+        roomButton.snp.makeConstraints {
+            $0.height.equalTo(56)
+            $0.width.equalTo(125)
+            $0.trailing.equalTo(view.snp.trailing).inset(0)
+            $0.bottom.equalTo(view.snp.bottom).inset(27)
+        }
     }
     
 }
