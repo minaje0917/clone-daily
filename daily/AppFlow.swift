49204258f068fcec1e50d83d35286ee5acf0bc49
minaje0917 <k050918@naver.com>
2023-01-30 02:26:09 +0900

    :zap: add MainStep

---
+++ daily/AppFlow.swift
@@ -0,0 +1,13 @@
+//
+//  Flow.swift
+//  daily
+//
+//  Created by 선민재 on 2023/01/30.
+//
+
+import RxFlow
+import UIKit
+
+class AppFlow: Flow {
+    pri
+}
---
+++ daily/MainStep.swift
@@ -0,0 +1,18 @@
+//
+//  MainStep.swift
+//  daily
+//
+//  Created by 선민재 on 2023/01/30.
+//
+
+import RxFlow
+
+enum MainStep: Step {
+    case loginIsRequired
+    case userIsLoggedIn
+    
+    case signUpIsRequired
+    case userIsSignUp
+    
+    case homeIsRequired
+}
