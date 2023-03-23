faaa8261db455492900a8d193a3e016a0afefa98
minaje0917 <k050918@naver.com>
2023-03-23 23:11:32 +0900

    :sparkles: Add:: SelectThemeCell

---
+++ daily/Sources/Presentation/Auth/SignUp/SelectTheme/Cell/SelectThemeCell.swift
@@ -0,0 +1,19 @@
+//
+//  SelectThemeCell.swift
+//  daily
+//
+//  Created by 선민재 on 2023/03/23.
+//
+
+import Foundation
+import UIKit
+import SnapKit
+import Then
+
+final class SelectThemeCell: UICollectionViewCell {
+    static let id = "SelectThemeCell"
+    
+    private let themeImage = UIImageView().then {
+        $0.image = UIImage
+    }
+}
