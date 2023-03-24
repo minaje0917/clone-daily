8c105d988a6d0450c1cc5e75474f63a801fcde86
minaje0917 <k050918@naver.com>
2023-03-24 09:30:08 +0900

    :sparkles: Add :: SelectThemeCell

--- daily/Sources/Presentation/Auth/SignUp/SelectTheme/Cell/SelectThemeCell.swift
+++ daily/Sources/Presentation/Auth/SignUp/SelectTheme/Cell/SelectThemeCell.swift
@@ -13,7 +13,25 @@ import Then
 final class SelectThemeCell: UICollectionViewCell {
     static let id = "SelectThemeCell"
     
-    private let themeImage = UIImageView().then {
-        $0.image = UIImage
+    private let themeView = UIView().then {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+
+    @available(*, unavailable)
+        required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.contentView.addSubview(self.themeView)
+        NSLayoutConstraint.activate([
+            self.themeView.leftAnchor.constraint(equalTo: self.contentView.leftAnchor),
+            self.themeView.rightAnchor.constraint(equalTo: self.contentView.rightAnchor),
+            self.themeView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor),
+            self.themeView.topAnchor.constraint(equalTo: self.contentView.topAnchor),
+        ])
+    }
+    
 }
