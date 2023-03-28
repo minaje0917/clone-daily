4453facd7c303ae4640aa3e9343cbc035ae3f8bd
minaje0917 <k050918@naver.com>
2023-03-28 10:30:14 +0900

    :ambulance: hotfilx

---
+++ daily/Sources/Presentation/Auth/SignUp/SelectTheme/VC/SeletThemeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SeletThemeViewController.swift
+//  daily
+//
+//  Created by 선민재 on 2023/03/24.
+//
+
+import UIKit
+
+class SeletThemeViewController: BaseViewController<SelectThemeViewModel> {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    private enum Const {
+//        static let itemSize = CGSize(
+//            width: ((bounds.width) / 1.51),
+//            height: ((bounds.height) / 1.52)
+//        )
+    }
+    
+//    override func addView() {
+//        <#code#>
+//    }
+//
+//    override func setLayout() {
+//        <#code#>
+//    }
+}
