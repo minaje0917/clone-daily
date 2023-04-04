45d36c95fd559976587d044c180b266a205f6fa7
minaje0917 <k050918@naver.com>
2023-04-04 15:09:23 +0900

    :sparkles: Add :: ThemeType

---
+++ daily/Sources/Domain/Enums/ThemeType.swift
@@ -0,0 +1,13 @@
+//
+//  ThemeType.swift
+//  daily
+//
+//  Created by 선민재 on 2023/04/04.
+//
+
+import Foundation
+
+public enum ThemeType: String, Codable, CaseIterable{
+    case Ocean
+    case GrassLand
+}
