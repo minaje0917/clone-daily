1d9cfef1d5432cffe68d627d221416b9fcc401a4
minaje0917 <k050918@naver.com>
2023-06-13 11:28:59 +0900

    :sparkles: Add :: GetDiaryDetail func

--- daily/Sources/Presentation/Main/Profile/VM/ProfileViewModel.swift
+++ daily/Sources/Presentation/Main/Profile/VM/ProfileViewModel.swift
@@ -14,7 +14,9 @@ import Moya
 class ProfileViewModel: BaseViewModel, Stepper{
     let keychain = Keychain()
     let profileProvider = MoyaProvider<ProfileServices>()
+    let detailProvider = MoyaProvider<DiaryServices>()
     lazy var accessToken = "Bearer " + (keychain.read(key: Const.KeychainKey.accessToken) ?? "")
+    var diaryData: DiaryDetailResponse?
     var listData: [GetDiaryListResponse] = []
 
 }
@@ -28,6 +30,36 @@ extension ProfileViewModel {
                 do {
                     self.listData = try result.map([GetDiaryListResponse].self)
                     print(self.listData)
+                    completion()
+                }catch(let err) {
+                    print(String(describing: err))
+                }
+                switch statusCode{
+                case 200..<300:
+                    print("200")
+                case 400:
+                    print("400")
+                case 401:
+                    print("401")
+                case 409:
+                    print("409")
+                default:
+                    print("ERROR")
+                }
+            case .failure(let err):
+                print(String(describing: err))
+            }
+        }
+    }
+    
+    func getDiaryDetail(date: String, completion: @escaping () -> Void) {
+        detailProvider.request(.detail(authorization: accessToken, date: date)) { response in
+            switch response {
+            case .success(let result):
+                let statusCode = result.statusCode
+                do {
+                    self.diaryData = try result.map(DiaryDetailResponse.self)
+                    print(self.diaryData ?? "")
                     completion()
                 }catch(let err) {
                     print(String(describing: err))
